ac342cb76ad1c908b5879b62eaf0a8d815fc8843
Julio Augusto <julioams@icloud.com>
2020-01-17 08:49:08 -0300

    add cors package

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const routes = require('./routes');
 
 dotenv.config()
@@ -14,6 +15,7 @@ mongoose.connect(process.env.MONGO_URI, {
 }).then(() => console.log('DB Connect'))
   .catch(err => console.error(err));
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
